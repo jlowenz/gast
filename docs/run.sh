2ccdb58af17ddfa1ba9cbf461ed7d0dd9f1b1615
Jason Owens <jason.l.owens.civ@mail.mil>
2018-05-16 10:11:09 -0400

    Edits to support ARL technote and form1 process (in progress)

--- docs/run.sh
+++ docs/run.sh
@@ -8,4 +8,4 @@ docker run --rm -it \
        -v $BASE_DIR/../src:/src \
        -e LOCAL_USER_ID=$(id -u) \
        -e LOCAL_GROUP_ID=$(id -g) \
-       docker-ict.di2e.net/arl-latex:1.2 $CMD
+       docker-ict.di2e.net/arl-latex:1.3 $CMD
